8aa5df1b9d756b71740978e3799177bd1205d362
Chris Buetti <crb4595@gmail.com>
2025-07-03 15:01:53 -0400

    Add gspread-wrapper package and update setup.py
    Add gspread-wrapper package with GSPREAD class for easier interaction with Google Sheets. Update setup.py to include the new package in the installation process.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/chrisbuetti/gspread-wrapper",
-    py_modules=["gspread_wrapper"],  # or use packages=find_packages() if it's a folder/module
+    packages=find_packages(),
     install_requires=[
         "gspread>=5.0.0",
         "pandas>=1.0.0",
